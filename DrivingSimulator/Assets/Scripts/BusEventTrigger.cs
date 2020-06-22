5edd299bb9f1d8d284fa031d2b8bb2cf38f8e95a
Marco Herzog <mherzog.mmt-b2017@fh-salzburg.ac.at>
2020-06-22 18:22:15 +0200

    added driving simulator unity project (by Christoph Birgmann)

---
+++ DrivingSimulator/Assets/Scripts/BusEventTrigger.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Author: Christoph Birgmann
+ * University of Applied Sciences Salzburg
+ * MMP3
+ */
+
+using UnityEngine;
+
+public class BusEventTrigger : HazardEventTrigger
+{
+	[SerializeField]
+	private float indicatorTime = 1.0f;
+
+	private bool displayIndicator = false;
+
+	void Update()
+	{
+		if (displayIndicator)
+		{
+			indicatorTime -= Time.deltaTime;
+
+			if (indicatorTime <= 0.0f)
+			{
+				carController.indicatorsOn = RCC_CarControllerV3.IndicatorsOn.Off;
+				displayIndicator = false;
+				aiController.enabled = true;
+			}
+		}
+	}
+
+	protected override void OnTriggerEnter(Collider other)
+	{
+		base.OnTriggerEnter(other);
+
+		carController.indicatorsOn = RCC_CarControllerV3.IndicatorsOn.Right;
+		aiController.enabled = false;
+
+		displayIndicator = true;
+	}
+}
---
+++ DrivingSimulator/Assets/Scripts/LightSign.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Author: Christoph Birgmann
+ * University of Applied Sciences Salzburg
+ * MMP3
+ */
+
+using UnityEngine;
+
+public class LightSign : MonoBehaviour
+{
+	[SerializeField]
+	private Light redLight;
+	[SerializeField]
+	private Light yellowLight;
+	[SerializeField]
+	private Light greenLight;
+
+	private const float YELLOW_LIGHT_TIME = 1.0f;
+
+	private float timeToSwitch = 0.0f;
+
+	private bool switchLights;
+
+	private void Update()
+	{
+		if (switchLights)
+		{
+			timeToSwitch += Time.deltaTime;
+
+			if (timeToSwitch >= YELLOW_LIGHT_TIME)
+			{
+				yellowLight.gameObject.SetActive(false);
+				greenLight.gameObject.SetActive(true);
+				switchLights = false;
+				timeToSwitch = 0.0f;
+			}
+		}
+	}
+
+	public void SwitchToGreen()
+	{
+		redLight.gameObject.SetActive(false);
+		yellowLight.gameObject.SetActive(true);
+		switchLights = true;
+	}
+
+}
